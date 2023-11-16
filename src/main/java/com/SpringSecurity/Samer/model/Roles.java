7a9332190ae0a691f0859b0153a0c76e51b91dc1
Samer-Ismael <samerismaeelbat2@hotmail.com>
2023-11-16 18:49:03 +0100

    Saving

--- src/main/java/com/SpringSecurity/Samer/model/Roles.java
+++ src/main/java/com/SpringSecurity/Samer/model/Roles.java
@@ -2,9 +2,6 @@ package com.SpringSecurity.Samer.model;
 
 import org.springframework.security.core.GrantedAuthority;
 
-import java.util.Collection;
-import java.util.Iterator;
-
 public enum Roles implements GrantedAuthority {
     ROLE_USER,
     ROLE_ADMIN;
