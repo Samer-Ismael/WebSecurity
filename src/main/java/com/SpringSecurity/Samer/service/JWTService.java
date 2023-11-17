3fd48c896ce5c9a08dafddfc6908241cd10a0c30
Samer-Ismael <samerismaeelbat2@hotmail.com>
2023-11-17 22:05:13 +0100

    Saving

--- src/main/java/com/SpringSecurity/Samer/service/JWTService.java
+++ src/main/java/com/SpringSecurity/Samer/service/JWTService.java
@@ -19,22 +19,28 @@ import java.util.function.Function;
 
 @Component
 public class JWTService {
-    public String generateToken(String username){
-        Map<String,Object> claims = new HashMap<>();
-        return createToken(claims,username);
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
     }
 
     private String createToken(Map<String, Object> claims, String username) {
-        return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(username)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration( new Date(System.currentTimeMillis() + 7*24*60*60*1000)) // 7 days
-                .signWith(getSecritkey(), SignatureAlgorithm.HS256)
-                .compact();
+        return Jwts.builder().setClaims(claims).setSubject(username).setIssuedAt(new Date(System.currentTimeMillis())).setExpiration(new Date(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000)) // 7 days
+                .signWith(getSecritkey(), SignatureAlgorithm.HS256).compact();
     }
 
+
     private Key getSecritkey() {
+        /*
+    You can add environment variable in your operating system
+    with the name SECRET_KEY and the value of your secret key.
+    If you want to store that key in a very secure place.
+    I will not do that, but you can do it.
+
+    Use System.getenv() to get the value of the key and use it :).
+    String secretKey = System.getenv("SECRET_KEY");
+     */
+
         byte[] key = Decoders.BASE64.decode("daa5ec3d56b653ffcde8470d7ba08d918dd5d27fd447542ff8dadb590f0cacff");
         return Keys.hmacShaKeyFor(key);
     }
@@ -44,25 +50,23 @@ public class JWTService {
         return authConfig.getAuthenticationManager();
     }
 
-    public String getUserNameFromToken (String token){
+    public String getUserNameFromToken(String token) {
         return extractClaims(token, Claims::getSubject);
     }
 
-    public Date getExpirationDateFromToken(String token){
+    public Date getExpirationDateFromToken(String token) {
         return extractClaims(token, Claims::getExpiration);
     }
+
     public <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
+
     private Claims extractAllClaims(String token) {
-        return Jwts
-                .parserBuilder()
-                .setSigningKey(getSecritkey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+        return Jwts.parserBuilder().setSigningKey(getSecritkey()).build().parseClaimsJws(token).getBody();
     }
+
     private Boolean isTokenExpired(String token) {
         return getExpirationDateFromToken(token).before(new Date());
     }
