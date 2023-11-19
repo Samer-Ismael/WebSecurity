06c61869a9d5a5c9e618130f51572e791c5100a6
Samer-Ismael <samerismaeelbat2@hotmail.com>
2023-11-19 15:32:35 +0100

    Saving ´

--- src/test/java/com/SpringSecurity/Samer/SamerApplicationTests.java
+++ src/test/java/com/SpringSecurity/Samer/SamerApplicationTests.java
@@ -1,13 +1,31 @@
 package com.SpringSecurity.Samer;
 
+import com.SpringSecurity.Samer.controller.UserController;
+import com.SpringSecurity.Samer.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SamerApplicationTests {
+
+	@Autowired
+	private UserController userController;
+
+	@MockBean
+	private UserService userService;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void userControllerLoads() {
+		assertNotNull(userController);
+	}
+
+	// Add more tests here...
 }
